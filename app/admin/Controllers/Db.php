a908d79edb8e20ab0c81bf5adc19e5a56cc09d68
guodongtech <34568478+guodongtech@users.noreply.github.com>
2020-08-03 16:25:57 +0800

    数据库备份

--- app/admin/Controllers/Db.php
+++ app/admin/Controllers/Db.php
@@ -72,6 +72,104 @@ class Db extends BaseController
 		];
 		return json_encode($data);
 	}
+	//删除数据备份文件
+	function del()
+	{
+		$post = post();
+		$files = $post['files'];
+		foreach($files as $key=>$value)
+		{
+			//带路径信息的文件名为非法操作 不报错，直接返回
+			if(stripos($value,'/')){
+				return false;
+			}
+			//只允许删除sql zip文件
+			if(stripos($value,'.sql')||stripos($value,'.zip')&& is_file($this->dir.'/'.$value))
+			{
+				unlink($this->dir.'/'.$value);
+			}
+			else
+			{
+				$data = [
+					"code" => 0,
+					"msg" => "非法操作！",
+				];	
+				return json_encode($data);
+			}
+		}
+		$data = [
+			"code" => 1,
+			"msg" => "操作成功",
+		];	
+		return json_encode($data);
+	}
+	//下载文件
+	function download($file)
+	{
+		header('Content-Type: text/html; charset=UTF-8');
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename='.basename($file));
+		readfile($this->dir.'/'.$file);
+	}
+	//打包下载
+	function packDownload()
+	{
+		$post = post();
+		$files = $post['files'];
+		if(class_exists('ZipArchive'))
+		{
+			$fileName = $this->fPrefix.'_'.date('Ymd_His').'.zip';
+			$zip = new \ZipArchive();
+			$zip->open($this->dir.'/'.$fileName,\ZIPARCHIVE::CREATE);
+			foreach($files as $file)
+			{
+				$attachfile = $this->dir.'/'.$file;
+				$zip->addFile($attachfile,basename($attachfile));
+			}
+			$zip->close();
+			$data = [
+				"code" => 1,
+				"msg" => "打包已完成，单击此处下载",
+				"filename" => $fileName,
+			];	
+			return json_encode($data);
+		}
+		else
+		{
+			$data = [
+				"code" => 0,
+				"msg" => "服务器不支持打包",
+			];	
+			return json_encode($data);
+		}
+	}
+	
+	public function upload(){
+		$file = $this->request->getFile('file');
+		
+		if (! $file->isValid())
+		{
+			$data = [
+				"code" => 0,
+				"msg" => $file->getError(),
+			];
+			return json_encode($data);
+		}else{ //移动文件
+			//$newName = $file->getRandomName(); //此方法会自动更改后缀，此处不能用
+			$newName = date('Ymd_His');
+			$uploadPath = $this->dir;
+			//创建目录并设置权限
+			$file->move($uploadPath, $newName.'.sql');
+			$name = $file->getName();
+			$data = [
+				"code" => 1,
+				"msg" => "上传成功",
+			];
+			return json_encode($data);
+		}
+		
+	}
     //表列表
     public function getTableList()
     {
