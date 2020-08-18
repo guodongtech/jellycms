737a624758a7cf23fe07ef79931d3aa3caa9f74d
zhaojiabin <745320293@qq.com>
2020-08-18 20:30:50 +0800

    后台轮播

--- app/home/Controllers/Push.php
+++ app/home/Controllers/Push.php
@@ -31,4 +31,47 @@ class Push extends BaseController
 		];
 		echo json_encode($rdata);
 	}
+	public function rollImg(){
+		return '<link rel="stylesheet" href="/template/default/css/swiper.min.css">
+				<script src="/template/default/js/swiper.min.js"></script>
+				<style type="text/css">
+				  .swiper-container{
+				    --swiper-pagination-color: #a8deee;
+				  }
+				</style>
+				<div class="swiper-container" style="width: 565px;height: 200px;">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<a href="http://www.jellycms.cn/" target="_blank">
+								<img src="/static/upload/20200818/1597735358_196e50f4b22bccc30e8b.jpg" title="http://www.jellycms.cn/" width="565px" height="200px" />
+							</a>
+						</div>
+						<div class="swiper-slide">
+							<a href="http://www.jellycms.cn/" target="_blank"><img src="/static/upload/20200818/1597735358_e1d128c5ecf66559d84d.jpg" title="http://www.jellycms.cn/" width="565px" height="200px" /></a>
+						</div>
+						<div class="swiper-slide">
+							<a href="http://www.jellycms.cn/" target="_blank"><img src="/static/upload/20200818/1597735359_8a1045190ef18bf9d5c9.jpg" title="http://www.jellycms.cn/" width="565px" height="200px" />
+							</a>
+						</div>
+						<div class="swiper-slide">
+						<a href="http://www.jellycms.cn/" target="_blank"><img src="/static/upload/20200818/1597735359_5fc7817dd6e54e251a1b.jpg" title="http://www.jellycms.cn/" width="565px" height="200px" />
+						</a>
+						</div>
+					</div>
+					<div class="swiper-pagination" slot="pagination"></div>
+				</div>
+				<script type="text/javascript">
+					var mySwiper = new Swiper(".swiper-container", {
+							loop : true,
+							pagination:{
+					          	el:".swiper-pagination",
+					          	clickable:true,
+					        },
+						    autoplay: {
+							  	disableOnInteraction: false, //手动滑动之后不打断播放
+							  	delay: 2888
+							},
+						})
+				</script>';
+	}
 }
