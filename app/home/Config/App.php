99a7add53731d0caa9b2c858d834dffb3154b5d7
guodongtech <34568478+guodongtech@users.noreply.github.com>
2020-08-20 14:51:08 +0800

    CSS微调apache 配置文件

--- app/home/Config/App.php
+++ app/home/Config/App.php
@@ -181,7 +181,7 @@ class App extends BaseConfig
 	|
 	*/
 	public $sessionDriver            = 'CodeIgniter\Session\Handlers\FileHandler';
-	public $sessionCookieName        = 'jelly_session';
+	public $sessionCookieName        = 'jellycms_session';
 	public $sessionExpiration        = 7200;
 	public $sessionSavePath          = WRITEPATH . 'session';
 	public $sessionMatchIP           = false;
