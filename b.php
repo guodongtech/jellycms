9a07878205d52c3113a51c68cc9a01e895e1ff1f
guodongtech <34568478+guodongtech@users.noreply.github.com>
2020-09-11 11:55:03 +0800

    66

--- b.php
+++ b.php
@@ -74,78 +74,128 @@
     {
 		//$cssName = basename($url);
 		$cssPath = dirname($url);
-		$cssSrc = array();
+		static $cssSrc = array();
     	$content = getUrl($url);
 		preg_match_all("/url\((.*)\)/U", $content, $cssbga);
 		foreach($cssbga[1] as $key=>$value){
 			if(preg_match("/(data:)/",$value))continue;
 			$value = preg_replace('/\?.*/i','',$value);//替换？及以后的字符
 			$cssSrcT = str_replace('"', '', $value);
-			$cssSrc[] = str_replace("'", '', $cssSrcT);
+			$cssSrcT = str_replace("'", '', $cssSrcT);
+			$absolutePath = relativeToAbsolute($url, $cssSrcT);
+			$cssSrc[] = $absolutePath;
+			if(preg_match("/(\.css)/i",$absolutePath)){
+				$cssSrc[] = getCssSrc($url);
+			}
 		}
+		$cssSrc = array_unique($cssSrc);
 		return $cssSrc;
     }
     //相对路径转绝对路径
 	function relativeToAbsolute($url, $relativeUrl){
 		$path = dirname($url);
-		
-		
+		$pathArray = explode('/', $path);
+		$relativeArray = explode('/', $relativeUrl);
+		foreach($relativeArray as $key=>$value){
+			if($value == '..'){
+				array_pop($pathArray);
+				unset($relativeArray[$key]);
+			}else if($value == '.'){
+				//./相对当前路径，此处直接删除
+				unset($relativeArray[$key]);
+			}
+		}
+		$absolute = implode('/', $pathArray).'/'.implode('/', $relativeArray);
 		return $absolute;
 	}
- 
-	$url = 'http://www.jellycms.cn';
-    $html = getUrl($url);//抓取页面
-	
-    //匹配src地址，图片或者js 可能存在不规则写法如资源链接未带引号 缺少空格
-    preg_match_all("/src=(.*)(?:\/>|>| )/U", $html, $src);
-	foreach($src[1] as $key=>$value){
-		$src[1][$key] = str_replace('"', '', $value);
-		$src[1][$key] = str_replace("'", '', $src[1][$key]);
-	}
-	
-    //匹配css地址 可能存在不规则写法 如缺少引号 
-    preg_match_all("/href=(.*)(?:\/>|>| )/U", $html, $css);
-	foreach($css[1] as $key=>$value){
-		$css[1][$key] = str_replace('"', '', $value);
-		$css[1][$key] = str_replace("'", '', $css[1][$key]);
-	}
 
-    //匹配网页中css背景图片 url()中背景图片可能有单引号  双引号 或者不带引号
-    preg_match_all("/url\((.*)\)/U", $html, $cssbg);
-	foreach($cssbg[1] as $key=>$value){
-		$cssbg[1][$key] = str_replace('"', '', $value);
-		$cssbg[1][$key] = str_replace("'", '', $cssbg[1][$key]);
-	}
+	
+	function getSrcUrlList($url){
+		$html = getUrl($url);//抓取页面
+		//匹配src地址，图片或者js 可能存在不规则写法如资源链接未带引号 缺少空格
+		preg_match_all("/src=(.*)(?:\/>|>| )/U", $html, $src);
+		foreach($src[1] as $key=>$value){
+			$src[1][$key] = str_replace('"', '', $value);
+			$src[1][$key] = str_replace("'", '', $src[1][$key]);
+			$src[1][$key] = relativeToAbsolute($url, $src[1][$key]);
+		}
+		
+		//匹配css地址 可能存在不规则写法 如缺少引号 
+		preg_match_all("/href=(.*)(?:\/>|>| )/U", $html, $css);
+		foreach($css[1] as $key=>$value){
+			$css[1][$key] = str_replace('"', '', $value);
+			$css[1][$key] = str_replace("'", '', $css[1][$key]);
+			$css[1][$key] = relativeToAbsolute($url, $css[1][$key]);
+		}
 
-    //合并结果数组
-    $urlList = array_merge($src[1],$css[1],$cssbg[1]);
-	$urlList = array_unique($urlList); //去掉重复值 
-    //保存页面html
-   // file_put_contents($fileName,preg_replace('/(http:\/\/img.yigouf.com)|(http:\/\/pfghouse.pinfangw.com)|(\/\/script.crazyegg.com)/i','',$html));
+		//匹配网页中css背景图片 url()中背景图片可能有单引号  双引号 或者不带引号
+		preg_match_all("/url\((.*)\)/U", $html, $cssbg);
+		foreach($cssbg[1] as $key=>$value){
+			$cssbg[1][$key] = str_replace('"', '', $value);
+			$cssbg[1][$key] = str_replace("'", '', $cssbg[1][$key]);
+			$cssbg[1][$key] = relativeToAbsolute($url, $cssbg[1][$key]);
+		}
+
+		//合并结果数组
+		$urlList = array_merge($src[1],$css[1],$cssbg[1]);
+		$urlList = array_unique($urlList); //去掉重复值 
+		//保存页面html
+	   // file_put_contents($fileName,preg_replace('/(http:\/\/img.yigouf.com)|(http:\/\/pfghouse.pinfangw.com)|(\/\/script.crazyegg.com)/i','',$html));
+		return $urlList = array_unique($urlList);		
+	}
+	function getAllSrc($urlList){
+		$staticList = array();
+		foreach($urlList as $item){
+			$path = dirname($item);
+			if(strpos($item,'script.crazyegg.com'))continue;
+			if(preg_match("/(\.js)|(\.css)|(\.jpg)|(\.png)|(\.gif)|(\.ico)/i",$item)){
+				if(preg_match(".baidu.",$item))continue;//排除百度代码
+				$newFile = preg_replace('/\?.*/i','',$item);//替换？及以后的字符
+				if(preg_match("/([(http:\/\/)(\/\/)])+.*\.com/i",$item))$newFile = preg_replace('/([(http:\/\/)(\/\/)])+.*\.com/i','',$newFile);//跨域资源存到本地的名字
+				if(preg_match("(//)",$item)){
+					$staticUrl = $item;
+				}else{
+					$staticUrl = $path.$item;
+				}
+				$staticList[] = $staticUrl;
+				//保存css文件中的背景图片资源
+				if(preg_match("/(\.css)/i",$item)){
+					 $staticList = array_merge(getCssSrc($staticUrl),$staticList);
+				}
+			}
+		}
+		return  array_unique($staticList);
+	}
+	
+	function getList(){
+			$srcUrlList = array();
+			//$urlList = $_POST['urllist'];
+			$urlList = [
+				['url'=>'http://www.jellycms.cn/'],
+				['url'=>'http://www.jellycms.cn/?aboutus/'],
+			];
+			foreach($urlList as $key=>$value){
+				$srcUrlList= array_merge(getSrcUrlList($value['url']), $srcUrlList);
+			}
+			$list = getAllSrc($srcUrlList);
 	 
-	
-	
-	$staticList = array();
-    //采集静态资源
-    foreach($urlList as $item){
-        if(strpos($item,'script.crazyegg.com'))continue;
-    	if(preg_match("/(\.js)|(\.css)|(\.jpg)|(\.png)|(\.gif)|(\.ico)/i",$item)){
-    		if(preg_match(".baidu.",$item))continue;//排除百度代码
-    		$newFile = preg_replace('/\?.*/i','',$item);//替换？及以后的字符
-    		if(preg_match("/([(http:\/\/)(\/\/)])+.*\.com/i",$item))$newFile = preg_replace('/([(http:\/\/)(\/\/)])+.*\.com/i','',$newFile);//跨域资源存到本地的名字
-    		if(preg_match("(//)",$item)){
-    			$staticUrl = $item;
-    		}else{
-    			$staticUrl = $url.$item;
-    		}
-			$staticList[] = $staticUrl;
-    		//保存css文件中的背景图片资源
-    		if(preg_match("/(\.css)/i",$item)){
-    			 $staticList = array_merge(getCssSrc($staticUrl),$staticList);
-    		}
-    	}
-    		
-    }
-   print_r($staticList);
+		foreach($list as $key=>$value){
+			$resList[] = [
+				'url' => $value,
+				'id' => $key+1,
+				'status' => 0,
+			];
+		}
+		//$resList = array_unique($resList);
+		$data = [
+			"code" => 0,
+			"msg" => "",
+			"count" => count($resList),
+			"data" => $resList,
+		];
+		return json_encode($data);
+	}
+	echo getList();
+ 
 
 ?>
